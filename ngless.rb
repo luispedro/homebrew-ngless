43fec34dc7b50209e56561a9cbeb8227f974f356
Luis Pedro Coelho <luis@luispedro.org>
2017-11-29 16:21:49 +0100

    ENH Update to v0.5.0

--- ngless.rb
+++ ngless.rb
@@ -1,13 +1,13 @@
 require "language/haskell"
 
 class Ngless < Formula
-  include Language::Haskell::Cabal
+  include Language::Haskell::Stack
 
   desc "Domain Specific Language for NGS Processing"
   homepage "http://ngless.readthedocs.io/"
-  url "https://github.com/luispedro/ngless/archive/9133f1443b5db854ca1ae819d6c7c88ad63232ea.zip"
-  sha256 "f00a64d3afeeae3ca7902c24770818b05d2645c0ffc1fdceb6d84e11002a81b1"
-  version "0.0.0"
+  url "https://github.com/luispedro/ngless/archive/v0.5.tar.gz"
+  sha256 "f02a68d3afeeae3ca7902c24770818b05d2645c0ffc1fdceb6d84e11002a81b1"
+  version "0.5.0"
 
   head "https://github.com/luispedro/ngless.git"
 
@@ -18,7 +18,6 @@ class Ngless < Formula
   depends_on "samtools" => :install
 
   def install
-    system "m4 NGLess.cabal.m4 > NGLess.cabal"
     install_cabal_package
   end
 end
